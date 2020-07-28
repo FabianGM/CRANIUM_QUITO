0eedbb28212e81e7a43805eb52505ad500a3fa6c
Verónica Pillajo <vpillajon@est.ups.edu.ec>
2020-07-28 18:35:37 -0500

    arreglos en acerca de

--- src/app/paginas/acerca/acerca.page.ts
+++ src/app/paginas/acerca/acerca.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-acerca',
@@ -7,34 +8,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AcercaPage implements OnInit {
 
+  constructor(public alertCtr: AlertController) { }
+
+  ngOnInit() {
+  }
+
   componentes: componentes[]=[
     {
     src: './assets/icon/churos.jpeg',
-    name: 'Fabián Sebastián Garrido Mena'
+    name: 'Fabián Sebastián Garrido Mena',
+    correo: 'fgarridom@est.ups.edu.ec',
+    telefono: '0982800451'
   },
   {
     src: './assets/icon/anthony.jpeg',
-    name: 'Anthony Xavier Cárdenas Tuza'
+    name: 'Anthony Xavier Cárdenas Tuza',
+    correo: 'acardenast@est.ups.edu.ec',
+    telefono: '0995689611'
   },
   {
     src: './assets/icon/vero1.jpg',
-    name: 'Verónica Estefania Pillajo Nogales'
+    name: 'Verónica Estefania Pillajo Nogales',
+    correo: 'vpillajon@est.ups.edu.ec',
+    telefono: '0987416510'
   }
   ,
   {
     src: './assets/icon/sebas.jpg',
-    name: 'S. D. Guandinango de la Cruz'
+    name: 'Sebastián Danilo Guandinango de la Cruz',
+    correo: 'sguandinangod@est.ups.edu.ec',
+    telefono: '0998452740'
   }
 ];
 
-  constructor() { }
+async presentAlert(personas) {
+  const alert = await this.alertCtr.create({
+  
+    message: `<ion-card>
+    <ion-img src=${personas.src} style="width: 60%;"></ion-img>
+    <p>Nombre:</p>
+    <p>${personas.name}</p>
+    <p>Correo:</p>
+    <p>${personas.correo}</p>
+    <p>Teléfono:</p>
+    <p>${personas.telefono}</p>
+    </ion-card>`,
+    backdropDismiss: false,
+    buttons: [
+      {
+        text: 'Cancelar',
+        role: 'cancel',
+        cssClass: 'secondary',
+        handler: (blah) => {
+          console.log('Confirm Cancel: blah');
+        }
+      }, {
+        text: 'Aceptar',
+        handler: () => {
+          console.log('Confirm Okay');
+        }
+      }
+    ]
+  });
 
-  ngOnInit() {
-  }
+  await alert.present();
+}
 
 }
 
 interface componentes{
   src: String;
-  name: String
+  name: String;
+  correo: String;
+  telefono: String;
 }
