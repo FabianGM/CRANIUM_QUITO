f5b03f9f440192670a2d8d284f51845b17b36377
AnthonyEC7 <acardenast@est.ups.edu.ec>
2020-07-30 20:58:11 -0500

    STORAGE ALEATORIO EQUIPO

--- src/app/paginas/juego-principal/juego-principal.page.ts
+++ src/app/paginas/juego-principal/juego-principal.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { Camera } from '@ionic-native/camera/ngx';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-juego-principal',
@@ -14,12 +15,32 @@ export class JuegoPrincipalPage implements OnInit {
   mostrar_dado = true;
   // tslint:disable-next-line: variable-name
   mostrar_texto = true;
-  src = './assets/icon/dado.jpg';
+  src = './assets/icon/dado.jpeg';
+
+
+  equipo = [];
+
 
   constructor(private alertCtrl: AlertController,
-              private camera: Camera) { }
+              private camera: Camera,
+              private storage: Storage) { }
 
   ngOnInit() {
+    this.storage.get('equipos').then((val) => {
+      console.log('Los equipos son: ', val);
+      // this.equipo = val;
+
+      const randomico = Math.floor((Math.random() * 0) + 1);
+
+      if (randomico === 0){
+        this.equipo = val.equipo1;
+      }else{
+        this.equipo = val.equipo2;
+      }
+
+      // console.log(randomico);
+      // console.log(this.equipo['equipo2']);
+    });
   }
 
   onClick(){
@@ -29,7 +50,45 @@ export class JuegoPrincipalPage implements OnInit {
 
   }
 
- 
+  async presentAlert() {
+    const alert = await this.alertCtrl.create({
+      header: ``,
+      backdropDismiss: false,
+      subHeader: ``,
+      message: `
+      <ion-list>
+          <ion-list-header>
+            <ion-label>
+             ¿GANADOR EQUIPO ${this.equipo}?
+            </ion-label>
+          </ion-list-header>
+          <ion-item>
+            <ion-select placeholder="Seleccione">
+              <ion-select-option value="yes">Si</ion-select-option>
+              <ion-select-option value="no">No</ion-select-option>
+            </ion-select>
+          </ion-item>
+        </ion-list>
+  `,
+      buttons: [
+        {
+          text: 'Continuar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            this.getCamera();
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+
+
+
+
+  }
+
 
 
   async alertCard() {
