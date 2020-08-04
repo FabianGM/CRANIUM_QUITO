17c131e67e9c95b0ef1867337eac54ee80d4f30f
SebasDany <sguandinangod@est.ups.edu.ec>
2020-08-03 19:43:45 -0500

    Cambio de colores en las tarjetas

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -40,84 +40,98 @@ console.log(this.val);
 datos=[
   {
     card:'Verde',
+    color:'success',
   descripcion:'Dibujar',
   pregunta:'Dibuje la característica principal  sobre leyenda de iglesia de la catedral de Quito',
   pista:'El gallo, El pingüino, Las monjas',
   respuesta:'El gallo'
     }, {
       card:'Verde',
+      color:'success',
     descripcion:'Dibujar',
     pregunta:'Dibuje al ser de la leyenda que contruyó la iglesia de San Francisco de Quito',
     pista:'El arquitecto,  Las monjas, El cura, El diablo',
     respuesta:'El diablo'
       }, {
         card:'Verde',
+        color:'success',
       descripcion:'Dibujar',
       pregunta:'Dibuje al ser de la leyenda que contruyó la iglesia de San Francisco de Quito',
       pista:'El arquitecto,  Las monjas, El cura, El diablo',
       respuesta:'El diablo'
         }, {
           card:'Verde',
+          color:'success',
         descripcion:'Dibujar',
         pregunta:'Dibuje el unico lugar en donde el huevo puede permancer parado',
         pista:'En la linea Ecuatorial,  En la pñaza de sanfrancisco , En la mitad del mundo',
         respuesta:'En la mitad del mundo'
           }, {
             card:'Verde',
+            color:'success',
           descripcion:'Dibujar',
           pregunta:'Dibuje algo que resalte mas entro del turismo en quito ',
           pista:'La arquitectura,  Utilizado como un santuario , Lugar donde se predica la palabra',
           respuesta:'Iglesia'
             }, {
               card:'Amarillo',
+              color:'warning',
             descripcion:'Actuar',
             pregunta:'Actúa como un soldado en la época colonial de Quito',
             pista:'Guerra,  Muerte , Esapada, Bandera',
             respuesta:'Guerra,  Muerte , Esapada, Bandera'
               }, {
                 card:'Amarillo',
+                color:'warning',
               descripcion:'Actuar',
               pregunta:'Actúa como si hubieras ganado la batalla contra los españoles',
               pista:'',
               respuesta:''
                 }, {
                   card:'Amarillo',
+                  color:'warning',
                 descripcion:'Actuar',
                 pregunta:'Represente la pose de la Virgen del Panecillo',
                 pista:'',
                 respuesta:''
                   }, {
                     card:'Rojo',
+                    color:'danger',
                   descripcion:'Moldear',
                   pregunta:'Intenta hacer la Cruz del Papa con la plastilina Cranium',
                   pista:'',
                   respuesta:''
                     }, {
                       card:'Rojo',
+                      color:'danger',
                     descripcion:'Moldear',
                     pregunta:'Intenta representar el Panecillo con la plastilina Cranium',
                     pista:'',
                     respuesta:''
                       }, {
                         card:'Rojo',
+                        color:'danger',
                       descripcion:'Moldear',
                       pregunta:'Intenta representar el estadio olímpico con la plastilina Cranium',
                       pista:'',
                       respuesta:''
                         }, {
                           card:'Rojo',
+                          color:'danger',
                         descripcion:'Moldear',
                         pregunta:'Intenta representar los túneles de San Diego con la plastilina Cranium',
                         pista:'',
                         respuesta:''
                           }, {
                             card:'Rojo',
+                            color:'danger',
                           descripcion:'Moldear',
                           pregunta:'Intenta representar la virgen del Panecillo',
                           pista:'',
                           respuesta:''
                             },{
               card:'Azul',
+              color:'primary',
     descripcion:'Significado',
     pregunta:' Escriba su respuesta en minúscula ¿Quito es la capital más antigua de Sudamérica? 1. Si 2. No',
     pista:'',
@@ -125,6 +139,7 @@ datos=[
       },
     {
       card:'Azul',
+      color:'primary',
     descripcion:'Significado',
     pregunta:' Escriba el número de opción correcta ¿Qué significa Quito la Carita de Dios? 1. Es una de las ciudades más cerca del cielo 2. Es una ciudad iluminada',
     pista:'',
@@ -132,6 +147,7 @@ datos=[
       },
       {
       card:'Azul',
+      color:'primary',
     descripcion:'Significado',
     pregunta:' Escriba el número de opción correcta ¿Cuál es el nombre completo de Quito? 1.Quito del segundo amanecer  2. San Francisco de Quito',
     pista:'',
@@ -140,6 +156,7 @@ datos=[
     
      {
       card:'Azul',
+      color:'primary',
     descripcion:'Significado',
     pregunta:' Escriba el número de opción correcta ¿Cuándo se celebra la fundación de Quito? 1. En Septiembre 24  2. En Diciembre 6',
     pista:'',
@@ -147,6 +164,7 @@ datos=[
       },
     {
       card:'Azul',
+      color:'primary',
     descripcion:'Significado',
     pregunta:' Escriba su respuesta en minúscula ¿Que leyenda es conocida como el atrio de San Francisco?',
     pista:'añutnaC',
@@ -155,6 +173,7 @@ datos=[
     
     {
       card:'Amarillo',
+      color:'warning',
     descripcion:'Actuar',
     pregunta:'Has como si ya no jalaras',
     pista:'En Quito ya no jalo significa que estás cansado',
@@ -164,6 +183,7 @@ datos=[
     
      {
       card:'Amarillo',
+      color:'warning',
     descripcion:'Actuar',
     pregunta:'Actua como si pidieras un taxi en Quito diciendo Jefe una carrerita',
     pista:'En Quito se llama al Chofer y Guardia Jefe',
@@ -173,6 +193,7 @@ datos=[
     
       {
       card:'Amarillo',
+      color:'warning',
     descripcion:'Actuar',
     pregunta:'Actua como un Quiteño mide el tiempo con sus amigos',
     pista:'En Quito a un tiempo largo se dice a se fuuuuuuuu intentalo con una frase que se te ocurra y hazlo con dos personas como una covnersación',
@@ -180,6 +201,7 @@ datos=[
       },
        {
       card:'Amarillo',
+      color:'warning',
     descripcion:'Actuar',
     pregunta:'Actua como un Quiteño dice chulla vida en un situación peligrosa',
     pista:'El Quiteño dice chulla vida haciendo referencia a lo que se conoce como YOLO',
@@ -187,6 +209,7 @@ datos=[
       },
         {
       card:'Amarillo',
+      color:'warning',
     descripcion:'Actuar',
     pregunta:'Actua de manera acholada con un persona',
     pista:'Acholada/o en un expresión quiteña que significa que siente timidez',
--- src/app/paginas/administrador/administrador.page.ts
+++ src/app/paginas/administrador/administrador.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { ModalController, ToastController } from '@ionic/angular';
 import { ModalInfoPage } from '../modal-info/modal-info.page';
 import { Storage } from '@ionic/storage';
 import { Observable } from 'rxjs';
@@ -16,7 +16,7 @@ export class AdministradorPage implements OnInit {
   fuente_base: string;
   cards: Observable<any>;
   allcards=[]
-  constructor(private modalCtrl:ModalController,private storage: Storage) { 
+  constructor(private modalCtrl:ModalController,private storage: Storage,public toastController: ToastController) { 
     this.fondo_base = localStorage.getItem('fondo');
     this.encabezado_base = localStorage.getItem('encabezado');
     this.pie_base = localStorage.getItem('pie');
@@ -67,84 +67,98 @@ export class AdministradorPage implements OnInit {
   datos=[
     {
       card:'Verde',
+      color:'success',
     descripcion:'Dibujar',
     pregunta:'Dibuje la característica principal  sobre leyenda de iglesia de la catedral de Quito',
     pista:'El gallo, El pingüino, Las monjas',
     respuesta:'El gallo'
       }, {
         card:'Verde',
+        color:'success',
       descripcion:'Dibujar',
       pregunta:'Dibuje al ser de la leyenda que contruyó la iglesia de San Francisco de Quito',
       pista:'El arquitecto,  Las monjas, El cura, El diablo',
       respuesta:'El diablo'
         }, {
           card:'Verde',
+          color:'success',
         descripcion:'Dibujar',
         pregunta:'Dibuje al ser de la leyenda que contruyó la iglesia de San Francisco de Quito',
         pista:'El arquitecto,  Las monjas, El cura, El diablo',
         respuesta:'El diablo'
           }, {
             card:'Verde',
+            color:'success',
           descripcion:'Dibujar',
           pregunta:'Dibuje el unico lugar en donde el huevo puede permancer parado',
           pista:'En la linea Ecuatorial,  En la pñaza de sanfrancisco , En la mitad del mundo',
           respuesta:'En la mitad del mundo'
             }, {
               card:'Verde',
+              color:'success',
             descripcion:'Dibujar',
             pregunta:'Dibuje algo que resalte mas entro del turismo en quito ',
             pista:'La arquitectura,  Utilizado como un santuario , Lugar donde se predica la palabra',
             respuesta:'Iglesia'
               }, {
                 card:'Amarillo',
+                color:'warning',
               descripcion:'Actuar',
               pregunta:'Actúa como un soldado en la época colonial de Quito',
               pista:'Guerra,  Muerte , Esapada, Bandera',
               respuesta:'Guerra,  Muerte , Esapada, Bandera'
                 }, {
                   card:'Amarillo',
+                  color:'warning',
                 descripcion:'Actuar',
                 pregunta:'Actúa como si hubieras ganado la batalla contra los españoles',
                 pista:'',
                 respuesta:''
                   }, {
                     card:'Amarillo',
+                    color:'warning',
                   descripcion:'Actuar',
                   pregunta:'Represente la pose de la Virgen del Panecillo',
                   pista:'',
                   respuesta:''
                     }, {
                       card:'Rojo',
+                      color:'danger',
                     descripcion:'Moldear',
                     pregunta:'Intenta hacer la Cruz del Papa con la plastilina Cranium',
                     pista:'',
                     respuesta:''
                       }, {
                         card:'Rojo',
+                        color:'danger',
                       descripcion:'Moldear',
                       pregunta:'Intenta representar el Panecillo con la plastilina Cranium',
                       pista:'',
                       respuesta:''
                         }, {
                           card:'Rojo',
+                          color:'danger',
                         descripcion:'Moldear',
                         pregunta:'Intenta representar el estadio olímpico con la plastilina Cranium',
                         pista:'',
                         respuesta:''
                           }, {
                             card:'Rojo',
+                            color:'danger',
                           descripcion:'Moldear',
                           pregunta:'Intenta representar los túneles de San Diego con la plastilina Cranium',
                           pista:'',
                           respuesta:''
                             }, {
                               card:'Rojo',
+                              color:'danger',
                             descripcion:'Moldear',
                             pregunta:'Intenta representar la virgen del Panecillo',
                             pista:'',
                             respuesta:''
                               },{
                 card:'Azul',
+                color:'primary',
       descripcion:'Significado',
       pregunta:' Escriba su respuesta en minúscula ¿Quito es la capital más antigua de Sudamérica? 1. Si 2. No',
       pista:'',
@@ -152,6 +166,7 @@ export class AdministradorPage implements OnInit {
         },
       {
         card:'Azul',
+        color:'primary',
       descripcion:'Significado',
       pregunta:' Escriba el número de opción correcta ¿Qué significa Quito la Carita de Dios? 1. Es una de las ciudades más cerca del cielo 2. Es una ciudad iluminada',
       pista:'',
@@ -159,6 +174,7 @@ export class AdministradorPage implements OnInit {
         },
         {
         card:'Azul',
+        color:'primary',
       descripcion:'Significado',
       pregunta:' Escriba el número de opción correcta ¿Cuál es el nombre completo de Quito? 1.Quito del segundo amanecer  2. San Francisco de Quito',
       pista:'',
@@ -167,6 +183,7 @@ export class AdministradorPage implements OnInit {
       
        {
         card:'Azul',
+        color:'primary',
       descripcion:'Significado',
       pregunta:' Escriba el número de opción correcta ¿Cuándo se celebra la fundación de Quito? 1. En Septiembre 24  2. En Diciembre 6',
       pista:'',
@@ -174,6 +191,7 @@ export class AdministradorPage implements OnInit {
         },
       {
         card:'Azul',
+        color:'primary',
       descripcion:'Significado',
       pregunta:' Escriba su respuesta en minúscula ¿Que leyenda es conocida como el atrio de San Francisco?',
       pista:'añutnaC',
@@ -182,6 +200,7 @@ export class AdministradorPage implements OnInit {
       
       {
         card:'Amarillo',
+        color:'warning',
       descripcion:'Actuar',
       pregunta:'Has como si ya no jalaras',
       pista:'En Quito ya no jalo significa que estás cansado',
@@ -191,6 +210,7 @@ export class AdministradorPage implements OnInit {
       
        {
         card:'Amarillo',
+        color:'warning',
       descripcion:'Actuar',
       pregunta:'Actua como si pidieras un taxi en Quito diciendo Jefe una carrerita',
       pista:'En Quito se llama al Chofer y Guardia Jefe',
@@ -200,6 +220,7 @@ export class AdministradorPage implements OnInit {
       
         {
         card:'Amarillo',
+        color:'warning',
       descripcion:'Actuar',
       pregunta:'Actua como un Quiteño mide el tiempo con sus amigos',
       pista:'En Quito a un tiempo largo se dice a se fuuuuuuuu intentalo con una frase que se te ocurra y hazlo con dos personas como una covnersación',
@@ -207,6 +228,7 @@ export class AdministradorPage implements OnInit {
         },
          {
         card:'Amarillo',
+        color:'warning',
       descripcion:'Actuar',
       pregunta:'Actua como un Quiteño dice chulla vida en un situación peligrosa',
       pista:'El Quiteño dice chulla vida haciendo referencia a lo que se conoce como YOLO',
@@ -214,13 +236,16 @@ export class AdministradorPage implements OnInit {
         },
           {
         card:'Amarillo',
+        color:'warning',
       descripcion:'Actuar',
       pregunta:'Actua de manera acholada con un persona',
       pista:'Acholada/o en un expresión quiteña que significa que siente timidez',
       respuesta:''
         }] ;
+  
   data={
     card:'',
+    color:'',
     descripcion:'',
     pregunta:'',
     pista:'',
@@ -234,6 +259,7 @@ export class AdministradorPage implements OnInit {
        
     this. data1={
       card:this.data.card,
+      color:this.data.color,
       descripcion:this.data.descripcion,
       pregunta:this.data.pregunta,
       
@@ -249,9 +275,12 @@ export class AdministradorPage implements OnInit {
        this.datos.push(this.data1);
     
     this.storage.set('datosg.json', this.datos);
-    
+    this.presentToastInsertCard()
+    setTimeout(() => {
+      window.location.reload();
+    }, 500);
+   //  
    
-    window.location.reload();
    
    
   }
@@ -275,6 +304,7 @@ export class AdministradorPage implements OnInit {
   onChange(c){
     if(c=='Rojo'){
       this.data.descripcion='Moldear'
+      this.data.color='danger'
       this.rojo = !this.rojo;
       this.verde = false;
       this.azul = false;
@@ -284,6 +314,7 @@ export class AdministradorPage implements OnInit {
 
     if(c=='Verde'){
       this.data.descripcion='Dibujo'
+      this.data.color='success'
       this.verde = !this.verde;
       this.rojo = false;
       this.azul = false;
@@ -293,6 +324,7 @@ export class AdministradorPage implements OnInit {
     }
     if(c=='Azul'){
       this.data.descripcion='Significado'
+      this.data.color='primary'
       this.azul = !this.azul;
       this.verde = false;
       this.rojo = false;
@@ -302,6 +334,7 @@ export class AdministradorPage implements OnInit {
     }
     if(c=='Amarillo'){
       this.data.descripcion='Actuar'
+      this.data.color='warning'
       this.amarillo = !this.rojo;
       this.verde = false;
       this.azul = false;
@@ -315,6 +348,13 @@ export class AdministradorPage implements OnInit {
 
 console.log(this.val);
 console.log(c);
+}
+async presentToastInsertCard() {
+  const toast = await this.toastController.create({
+    message: 'Targeta ingresada correctamente',
+    duration: 3000
+  });
+  toast.present();
 }
   
 }
